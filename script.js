1d95488d314dee5f723b42298a2a1b6ba7ee1c05
Ramadoss3427 <ramadoss3427@gmail.com>
2023-12-29 03:01:54 -0800

    day 5 task completed

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//question 1
+       //for loop
 var arr=[
     {
         "name":"ram",
@@ -7,10 +9,12 @@ var arr=[
         "name":"raja",
         "age":"30"
     }
-]  
+]
  for(var i=0;i<arr.length;i++){
     console.log(arr[i].name,arr[i].age)
  }
+
+      //for in loop
  let personDetails={
     "name":"ravi",
     "age":35,
@@ -20,6 +24,30 @@ var arr=[
 for(let i in personDetails){
     console.log(i)
 }
+
+//for of loop
+const obj={
+    name:"gokul",
+    age:25,
+    work:"web developer",
+    place:"salem"
+}
+console.log(Object.keys(obj))
+for(const val of (Object.keys(obj)))
+console.log(val, "=>",obj[val])
+
+// for each loop
+const jsonArray = [
+    { name: 'John', age: 25 },
+    { name: 'ranjith', age: 30 },
+    { name: 'vijay', age: 35 }
+  ];
+  
+  jsonArray.forEach((jsonObject) => {
+     const name = jsonObject.name;
+    const age = jsonObject.age;
+  console.log(`Name: ${name}, Age: ${age}`);
+  });
 
 
 
